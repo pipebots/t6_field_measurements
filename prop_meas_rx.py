34f839eb808bf392e9af3bc61658e2128f20c706
Viktor Doychinov <v.o.doychinov@leeds.ac.uk>
2021-01-26 14:53:54 +0000

    Added logging and improved parameters
    Tidied up the scripts and added NTP sync and actual logging
    functionality. Everything is a parameter at the top of the file now,
    and individual filenames are built in a better way.

--- prop_meas_rx.py
+++ prop_meas_rx.py
@@ -1,40 +1,180 @@
+import os
 import time
+import datetime
+import logging
+import ntplib
 import numpy as np
 import adi
 
 
+def setup_logger(filename_base: str, timestamp: str) -> logging.Logger:
+    """Sets up a `Logger` object for diagnostic and debug
+
+    A standard function to set up and configure a Python `Logger` object
+    for recording diagnostic and debug data.
+
+    Args:
+        filename_base: A `str` containing a user-supplied filename to better
+                      identify the logs.
+        timestamp: A `str` with the date and time the logger was started
+                   to differentiate between different runs
+
+    Returns:
+        A `Logger` object with appropriate configurations. All the messages
+        are duplicated to the command prompt as well.
+
+    Raises:
+        Nothing
+    """
+    log_filename = "_".join([timestamp, filename_base])
+    log_filename = os.extsep.join([log_filename, "log"])
+
+    logger = logging.getLogger(filename_base)
+
+    logger_handler = logging.FileHandler(log_filename)
+    logger_handler.setLevel(logging.DEBUG)
+
+    fmt_string = "{asctime:s} {msecs:.3f} \t {levelname:^10s} \t {message:s}"
+    datefmt_string = "%Y-%m-%d %H:%M:%S"
+    logger_formatter = logging.Formatter(
+        fmt=fmt_string, datefmt=datefmt_string, style="{"
+    )
+
+    # * This is to ensure consistent formatting of the miliseconds field
+    logger_formatter.converter = time.gmtime
+
+    logger_handler.setFormatter(logger_formatter)
+    logger.addHandler(logger_handler)
+
+    # * This enables the streaming of messages to stdout
+    logging.basicConfig(
+        format=fmt_string,
+        datefmt=datefmt_string,
+        style="{",
+        level=logging.DEBUG,
+    )
+    logger.info("Logger configuration done")
+
+    return logger
+
+
+def log_ntp_time(logger: logging.Logger, ntp_server: str) -> None:
+    """A helper function to record the time as received from an NTP server
+
+    Establishes a connection with a specified NTP server, receives the time,
+    and logs it in both UTC and local time zones. When connecting to the
+    NTP server there is a chance that no response is received. In this case
+    the function will sleep (blocking) for three (3) seconds before attempting
+    again. After three (3) retries with no response the function will log an
+    error message and return.
+
+    Args:
+        logger: The `logging.Logger` object configured by `setup_logger`
+        ntp_server: A `str` with the web address of an NTP server
+
+    Returns:
+        None, the time received from the NTP server is recorded in the log
+        file and on the screen
+
+    Raises:
+        Nothing
+    """
+
+    ntp_client = ntplib.NTPClient()
+    ntp_connected = False
+    ntp_retries = 0
+
+    while not ntp_connected:
+        try:
+            ntp_time = ntp_client.request(ntp_server)
+        except ntplib.NTPException:
+            logger.warning("No response from NTP server, sleeping for 3 sec")
+            time.sleep(3)
+            ntp_retries -= 1
+        else:
+            ntp_connected = True
+            logger.info("Got response from NTP server")
+
+            ntp_time_utc = ntp_time.dest_timestamp
+            ntp_time_utc = datetime.datetime.fromtimestamp(ntp_time_utc)
+            ntp_time_utc = ntp_time_utc.strftime("%Y-%m-%d %H:%M:%S %f")
+
+            ntp_time_ltz = ntp_time.dest_time
+            ntp_time_ltz = datetime.datetime.fromtimestamp(ntp_time_ltz)
+            ntp_time_ltz = ntp_time_ltz.strftime("%Y-%m-%d %H:%M:%S %f")
+
+            logger.info(f"NTP UTC time: {ntp_time_utc}")
+            logger.info(f"NTP Local timezone time: {ntp_time_ltz}")
+
+        if 0 >= ntp_retries:
+            ntp_connected = True
+            logger.error("Could not perform NTP sync")
+
+
+EXPERIMENT_NAME = "ICAIR_Short_Sand_Rx"
+NTP_SERVER = "0.uk.pool.ntp.org"
 SDR_URI = "ip:plutobob.local"
 
-rx_lo_freq = int(2.45e9)
-rx_rf_bw = int(500e3)
-samp_rate = int(1024000)
-rx_buf_size = int(32768)
+RX_LO_FREQ_HZ = int(2.45e9)
+RX_RF_BW_HZ = int(500e3)
+RX_GAIN_DB = int(20)
 
-filename_base = "meas_100"
-tx_pol = "H"
-rx_pol = "H"
+SAMPLE_RATE = int(1024000)
+RX_BUFFER_SIZE = int(32768)
 
-num_meas = 500
+DDS_FREQ_HZ = int(200e3)
 
-pluto = adi.Pluto(SDR_URI)
-ad9361_phy = pluto._ctrl
-tx_lo = ad9361_phy.find_channel("TX_LO")
-tx_lo.attrs["powerdown"].value = str(int(1))
+TX_POL = "H"
+RX_POL = "H"
 
-pluto.rx_lo = rx_lo_freq
-pluto.rx_rf_bandwidth = rx_rf_bw
-pluto.sample_rate = samp_rate
-pluto.rx_buffer_size = rx_buf_size
+NUM_MEAS = 500
 
-pluto.gain_control_mode_chan0 = "manual"
-pluto.rx_hardwaregain_chan0 = 20
+meas_filename = "_".join(["meas", str(DDS_FREQ_HZ), str(RX_LO_FREQ_HZ),
+                          str(RX_GAIN_DB), TX_POL, RX_POL])
 
-for idx in range(num_meas):
-    filename = '_'.join([filename_base, str(rx_lo_freq), str(20),
-                         tx_pol, rx_pol, str(idx)])
-    filename = '.'.join([filename, 'iqbin'])
-    samples = pluto.rx()
-    samples = samples.astype(np.complex64)
-    samples.tofile(filename)
-    print(f"Wrote data: {filename}")
-    time.sleep(1)
+if __name__ == "__main__":
+    global_timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    sdr_rx_logger = setup_logger(EXPERIMENT_NAME, global_timestamp)
+    log_ntp_time(sdr_rx_logger, NTP_SERVER)
+
+    pluto = adi.Pluto(SDR_URI)
+    sdr_rx_logger.info(f"Connected to {pluto._device_name}")
+
+    ad9361_phy = pluto._ctrl
+    tx_lo = ad9361_phy.find_channel("TX_LO")
+    tx_lo.attrs["powerdown"].value = str(int(1))
+    sdr_rx_logger.info("Tx LO powered down on Rx side")
+
+    pluto.rx_lo = RX_LO_FREQ_HZ
+    sdr_rx_logger.info(f"Rx LO set to {pluto.rx_lo} Hz")
+
+    pluto.rx_rf_bandwidth = RX_RF_BW_HZ
+    sdr_rx_logger.info(f"Rx RF bandwidth set to {pluto.rx_rf_bandwidth} Hz")
+
+    pluto.sample_rate = SAMPLE_RATE
+    sdr_rx_logger.info(f"ADC set to {pluto.sample_rate} samples per second")
+
+    pluto.rx_buffer_size = RX_BUFFER_SIZE
+    sdr_rx_logger.info(f"Rx buffer size set to {pluto.rx_buffer_size} samples")
+
+    pluto.gain_control_mode_chan0 = "manual"
+    pluto.rx_hardwaregain_chan0 = RX_GAIN_DB
+    sdr_rx_logger.info(f"Rx gain set to {pluto.rx_hardwaregain_chan0} dB")
+
+    for idx in range(NUM_MEAS):
+        filename = '_'.join([meas_filename, str(idx)])
+        filename = '.'.join([filename, 'iqbin'])
+
+        samples = pluto.rx()
+        samples = samples.astype(np.complex64)
+        samples.tofile(filename)
+
+        sdr_rx_logger.info(f"Measurement {idx} out of {NUM_MEAS} complete")
+
+        time.sleep(1)
+
+    logging.shutdown()
+
+else:
+    print("Please run this script from the command prompt")
--- prop_meas_tx.py
+++ prop_meas_tx.py
@@ -1,21 +1,150 @@
+import os
+import time
+import datetime
+import logging
+import ntplib
 import adi
 
 
-pluto = adi.Pluto("ip:plutoalice.local")
+def setup_logger(filename_base: str, timestamp: str) -> logging.Logger:
+    """Sets up a `Logger` object for diagnostic and debug
 
-tx_lo_freq = int(2.45e9)
-tx_rf_bw = int(500e3)
+    A standard function to set up and configure a Python `Logger` object
+    for recording diagnostic and debug data.
 
-# -20 for all except 5.8 GHz, -10 then
-tx_gain = int(-20)
+    Args:
+        filename_base: A `str` containing a user-supplied filename to better
+                      identify the logs.
+        timestamp: A `str` with the date and time the logger was started
+                   to differentiate between different runs
 
-dds_freq = int(100e3)
-dds_scale = 1.0
+    Returns:
+        A `Logger` object with appropriate configurations. All the messages
+        are duplicated to the command prompt as well.
 
-pluto.tx_lo = tx_lo_freq
-pluto.tx_rf_bandwidth = tx_rf_bw
-pluto.tx_hardwaregain_chan0 = tx_gain
+    Raises:
+        Nothing
+    """
+    log_filename = "_".join([timestamp, filename_base])
+    log_filename = os.extsep.join([log_filename, "log"])
 
-pluto.dds_single_tone(dds_freq, dds_scale)
+    logger = logging.getLogger(filename_base)
 
-print(f"DDS tone set to {dds_freq}")
+    logger_handler = logging.FileHandler(log_filename)
+    logger_handler.setLevel(logging.DEBUG)
+
+    fmt_string = "{asctime:s} {msecs:.3f} \t {levelname:^10s} \t {message:s}"
+    datefmt_string = "%Y-%m-%d %H:%M:%S"
+    logger_formatter = logging.Formatter(
+        fmt=fmt_string, datefmt=datefmt_string, style="{"
+    )
+
+    # * This is to ensure consistent formatting of the miliseconds field
+    logger_formatter.converter = time.gmtime
+
+    logger_handler.setFormatter(logger_formatter)
+    logger.addHandler(logger_handler)
+
+    # * This enables the streaming of messages to stdout
+    logging.basicConfig(
+        format=fmt_string,
+        datefmt=datefmt_string,
+        style="{",
+        level=logging.DEBUG,
+    )
+    logger.info("Logger configuration done")
+
+    return logger
+
+
+def log_ntp_time(logger: logging.Logger, ntp_server: str) -> None:
+    """A helper function to record the time as received from an NTP server
+
+    Establishes a connection with a specified NTP server, receives the time,
+    and logs it in both UTC and local time zones. When connecting to the
+    NTP server there is a chance that no response is received. In this case
+    the function will sleep (blocking) for three (3) seconds before attempting
+    again. After three (3) retries with no response the function will log an
+    error message and return.
+
+    Args:
+        logger: The `logging.Logger` object configured by `setup_logger`
+        ntp_server: A `str` with the web address of an NTP server
+
+    Returns:
+        None, the time received from the NTP server is recorded in the log
+        file and on the screen
+
+    Raises:
+        Nothing
+    """
+
+    ntp_client = ntplib.NTPClient()
+    ntp_connected = False
+    ntp_retries = 0
+
+    while not ntp_connected:
+        try:
+            ntp_time = ntp_client.request(ntp_server)
+        except ntplib.NTPException:
+            logger.warning("No response from NTP server, sleeping for 3 sec")
+            time.sleep(3)
+            ntp_retries -= 1
+        else:
+            ntp_connected = True
+            logger.info("Got response from NTP server")
+
+            ntp_time_utc = ntp_time.dest_timestamp
+            ntp_time_utc = datetime.datetime.fromtimestamp(ntp_time_utc)
+            ntp_time_utc = ntp_time_utc.strftime("%Y-%m-%d %H:%M:%S %f")
+
+            ntp_time_ltz = ntp_time.dest_time
+            ntp_time_ltz = datetime.datetime.fromtimestamp(ntp_time_ltz)
+            ntp_time_ltz = ntp_time_ltz.strftime("%Y-%m-%d %H:%M:%S %f")
+
+            logger.info(f"NTP UTC time: {ntp_time_utc}")
+            logger.info(f"NTP Local timezone time: {ntp_time_ltz}")
+
+        if 0 >= ntp_retries:
+            ntp_connected = True
+            logger.error("Could not perform NTP sync")
+
+
+EXPERIMENT_NAME = "ICAIR_Short_Sand_Tx"
+NTP_SERVER = "0.uk.pool.ntp.org"
+SDR_URI = "ip:plutoalice.local"
+
+TX_LO_FREQ_HZ = int(2.45e9)
+TX_RF_BW_HZ = int(500e3)
+TX_GAIN_DB = int(-20)
+
+DDS_FREQ_HZ = int(200e3)
+DDS_SCALE = 1.0
+
+if __name__ == "__main__":
+    global_timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    sdr_tx_logger = setup_logger(EXPERIMENT_NAME, global_timestamp)
+    log_ntp_time(sdr_tx_logger, NTP_SERVER)
+
+    pluto = adi.Pluto(SDR_URI)
+    sdr_tx_logger.info(f"Connected to {pluto._device_name}")
+
+    pluto.tx_lo = TX_LO_FREQ_HZ
+    sdr_tx_logger.info(f"Tx LO set to {pluto.tx_lo} Hz")
+
+    pluto.tx_rf_bandwidth = TX_RF_BW_HZ
+    sdr_tx_logger.info(f"Tx RF bandwidth set to {pluto.tx_rf_bandwidth} Hz")
+
+    pluto.tx_hardwaregain_chan0 = TX_GAIN_DB
+    sdr_tx_logger.info(f"Tx attenuation set to"
+                       f" {pluto.tx_hardwaregain_chan0} dB")
+
+    pluto.dds_single_tone(DDS_FREQ_HZ, DDS_SCALE)
+    sdr_tx_logger.info(f"DDS frequency set to {DDS_FREQ_HZ} Hz")
+    sdr_tx_logger.info(f"DDS scale set to {DDS_SCALE}")
+
+    logging.shutdown()
+
+else:
+    print("Please run this script from the command prompt")
