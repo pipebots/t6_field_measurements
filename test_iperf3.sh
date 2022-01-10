1ecee9b8dde6ec24eab872d8e77b523aa17aa071
Viktor Doychinov <v.o.doychinov@leeds.ac.uk>
2022-01-10 17:44:13 +0000

    Changed the ordering of TCP and UDP

--- test_iperf3.sh
+++ test_iperf3.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-IPERF3_SERVER="8.8.8.8"
+IPERF3_SERVER=$1
+IPERF3_LOGFILE=$2
+
 IPERF3_SERVER_PORT=6969
-IPERF3_LOGFILE="node_2.log"
 IPERF3_TIME=60
 IPERF3_PACKET_SIZES=(32 160 288)
 IPERF3_BWS=(20000 70000 100000)
@@ -20,15 +21,20 @@ for BW in "${IPERF3_BWS[@]}"
 do
     echo "Testing with $BW bits per second"
     echo
+    echo "TCP Packets"
+    echo
     for PACKET_SIZE in "${IPERF3_PACKET_SIZES[@]}"
     do
         echo "Using $PACKET_SIZE payload"
         echo
-        echo "TCP Packets"
-        echo
         sudo iperf3 --port $IPERF3_SERVER_PORT --format k --client $IPERF3_SERVER --verbose --bandwidth $BW --length $PACKET_SIZE --time $IPERF3_TIME |& tee -a $IPERF3_LOGFILE
         sleep 3
-        echo "UDP Packets"
+    done
+    echo "UDP Packets"
+    echo
+    for PACKET_SIZE in "${IPERF3_PACKET_SIZES[@]}"
+    do
+        echo "Using $PACKET_SIZE payload"
         echo
         sudo iperf3 --port $IPERF3_SERVER_PORT --format k --client $IPERF3_SERVER --verbose --bandwidth $BW --length $PACKET_SIZE --time $IPERF3_TIME --udp |& tee -a $IPERF3_LOGFILE
         sleep 3
