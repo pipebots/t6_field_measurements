fe7f9703c1e3efdcac6197ade2114841ae0a5c13
Viktor Doychinov <eenvdo@leeds.ac.uk>
2022-01-27 16:58:19 +0000

    Initial version of ping and iperf3 test remote execution

--- ot_experiment.py
+++ ot_experiment.py
@@ -14,6 +14,7 @@ In the future this script will be extended to remotely run the `pink`,
 `iperf3`, and other experiments with network traffic.
 """
 
+from itertools import product
 import time
 import datetime
 import logging
@@ -137,7 +138,8 @@ ot_logger = helpers.setup_logger(
 )
 helpers.log_ntp_time(ot_logger, NTP_SERVER)
 
-ntwk_name = f"icair-{global_timestamp.strftime('%d%m%y')}"
+day_timestamp = global_timestamp.strftime('%d%m%y')
+ntwk_name = f"icair-{day_timestamp}"
 
 # * Establish remote connections to the Pis. Credentials are hard-coded in the
 # * first instance.
@@ -177,11 +179,11 @@ ot_logger.info("PlutoPi2 - OT Router 1 - Initialised")
 
 reset_ot_node(ot_router_2)
 log_ot_counters(ot_logger, ot_router_2)
-ot_logger.info("PlutoPi2 - OT Router 2 - Initialised")
+ot_logger.info("PlutoPi3 - OT Router 2 - Initialised")
 
 reset_ot_node(ot_router_3)
 log_ot_counters(ot_logger, ot_router_3)
-ot_logger.info("PlutoPi2 - OT Router 3 - Initialised")
+ot_logger.info("PlutoPi4 - OT Router 3 - Initialised")
 
 # * One of the OpenThread nodes must form the network, becoming its `leader`.
 # * This should be possible all the time, but in case there is an issue with
@@ -268,6 +270,10 @@ while True:
         router_1_ipv6 = response.split(" ")[-1][1:-1]
         ot_logger.info(f"Router 1 Mesh Local IPv6 address: {router_1_ipv6}")
         break
+    else:
+        ot_logger.info(f"Current Router 1 Node State: {node_state}")
+        ot_logger.info(f"Current Router 1 Node Type: {node_type}")
+        ot_logger.info("Waiting for 30 seconds...")
 
 ot_leader.send_command("sudo wpanctl permit-join --network-wide")
 time.sleep(5)
@@ -285,6 +291,10 @@ while True:
         router_2_ipv6 = response.split(" ")[-1][1:-1]
         ot_logger.info(f"Router 2 Mesh Local IPv6 address: {router_2_ipv6}")
         break
+    else:
+        ot_logger.info(f"Current Router 2 Node State: {node_state}")
+        ot_logger.info(f"Current Router 2 Node Type: {node_type}")
+        ot_logger.info("Waiting for 30 seconds...")
 
 ot_leader.send_command("sudo wpanctl permit-join --network-wide")
 time.sleep(5)
@@ -302,6 +312,10 @@ while True:
         router_3_ipv6 = response.split(" ")[-1][1:-1]
         ot_logger.info(f"Router 3 Mesh Local IPv6 address: {router_3_ipv6}")
         break
+    else:
+        ot_logger.info(f"Current Router 3 Node State: {node_state}")
+        ot_logger.info(f"Current Router 3 Node Type: {node_type}")
+        ot_logger.info("Waiting for 30 seconds...")
 
 # ! This series of commands makes sure the linear mesh topology is established.
 # ! Useful when running tests in free space in the lab, but also helps at
@@ -351,6 +365,8 @@ ot_leader.send_command("sudo wpanctl set MAC:Allowlist:Enabled true")
 ot_router_1.send_command("sudo wpanctl set MAC:Allowlist:Enabled true")
 ot_router_2.send_command("sudo wpanctl set MAC:Allowlist:Enabled true")
 ot_router_3.send_command("sudo wpanctl set MAC:Allowlist:Enabled true")
+
+ot_logger.info("MAC filtering enabled, waiting for 180 seconds...")
 time.sleep(180)
 
 ot_logger.info("Leader packet counters, neighbour table, and error rates")
@@ -370,5 +386,157 @@ log_ot_counters(ot_logger, ot_router_3)
 log_ot_neighbour_table(ot_logger, ot_router_3)
 
 ot_logger.info("OpenThread linear mesh network setup complete")
+
+# * Common variables for all tests
+routers = [ot_router_1, ot_router_2, ot_router_3]
+
+# * Ping tests
+ping_packet_sizes = [16, 32, 64, 128, 256, 512, 1024]
+ping_count = 100
+
+ot_logger.info("Running latency measurements using ping")
+
+for router in routers:
+    router_name = router.host.split('.')[0]
+    result_filename = f"ping_{router_name}_{day_timestamp}.log"
+    for pkt_size in ping_packet_sizes:
+        ping_cmd = (
+            f"ping -6 -I wpan0 -c {ping_count} -s {pkt_size} {leader_ipv6} "
+            f"|& tee -a {result_filename}"
+        )
+
+        ot_logger.info(f"Pinging from {router_name} with {pkt_size} payload")
+
+        response = router.send_command(ping_cmd)
+
+        with open(result_filename, "a") as fout:
+            fout.write(response)
+
+        ot_logger.info(
+            "Leader packet counters, neighbour table, and error rates"
+        )
+        log_ot_counters(ot_logger, ot_leader)
+        log_ot_neighbour_table(ot_logger, ot_leader)
+
+        ot_logger.info(
+            f"Router {router_name} counters, neighbours table, and error rates"
+        )
+        log_ot_counters(ot_logger, router)
+        log_ot_neighbour_table(ot_logger, router)
+
+        time.sleep(5)
+
+ot_logger.info("Finished latency tests")
+
+# * Common for iperf3 tests
+iperf3_port = 2607
+iperf3_time = 60
+iperf3_packet_sizes = [32, 160, 288, 416, 544, 672, 800, 928]
+iperf3_bandwidths = [20000, 70000, 100000]
+
+iperf3_server_log = f"iperf3_server_{day_timestamp}.log"
+ot_leader.send_command(
+    f"sudo iperf3 --server --daemon --verbose --port {iperf3_port} "
+    f"--logfile {iperf3_server_log}"
+)
+ot_logger.info("iperf3 server started on OpenThread leader node")
+
+# * TCP tests
+ot_logger.info("Running throughput tests using TCP")
+
+for router in routers:
+    router_name = router.host.split('.')[0]
+    result_filename = f"iperf3_{router_name}_{day_timestamp}.log"
+    for pkt_size, bw in product(iperf3_packet_sizes, iperf3_bandwidths):
+        iperf3_cmd = (
+            f"sudo iperf3 --client {leader_ipv6} --verbose --format k "
+            f"--port {iperf3_port} --bandwidth {bw} --length {pkt_size} "
+            f"--time {iperf3_time} |& tee -a {result_filename}"
+        )
+
+        ot_logger.info(
+            f"Transmitting from {router_name} at {bw} bps with {pkt_size} bytes"
+        )
+
+        response = router.send_command(iperf3_cmd)
+
+        with open(result_filename, "a") as fout:
+            fout.write(response)
+
+        ot_logger.info(
+            "Leader packet counters, neighbour table, and error rates"
+        )
+        log_ot_counters(ot_logger, ot_leader)
+        log_ot_neighbour_table(ot_logger, ot_leader)
+
+        ot_logger.info(
+            f"Router {router_name} counters, neighbours table, and error rates"
+        )
+        log_ot_counters(ot_logger, router)
+        log_ot_neighbour_table(ot_logger, router)
+
+        time.sleep(5)
+
+ot_logger.info("Finished TCP throughput tests")
+
+# * UDP tests
+ot_logger.info("Running throughput tests using UDP")
+
+for router in routers:
+    router_name = router.host.split('.')[0]
+    result_filename = f"iperf3_{router_name}_{day_timestamp}.log"
+    for pkt_size, bw in product(iperf3_packet_sizes, iperf3_bandwidths):
+        iperf3_cmd = (
+            f"sudo iperf3 --client {leader_ipv6} --verbose --format k --udp "
+            f"--port {iperf3_port} --bandwidth {bw} --length {pkt_size} "
+            f"--time {iperf3_time} |& tee -a {result_filename}"
+        )
+
+        ot_logger.info(
+            f"Transmitting from {router_name} at {bw} bps with {pkt_size} bytes"
+        )
+
+        response = router.send_command(iperf3_cmd)
+
+        with open(result_filename, "a") as fout:
+            fout.write(response)
+
+        ot_logger.info(
+            "Leader packet counters, neighbour table, and error rates"
+        )
+        log_ot_counters(ot_logger, ot_leader)
+        log_ot_neighbour_table(ot_logger, ot_leader)
+
+        ot_logger.info(
+            f"Router {router_name} counters, neighbours table, and error rates"
+        )
+        log_ot_counters(ot_logger, router)
+        log_ot_neighbour_table(ot_logger, router)
+
+        time.sleep(5)
+
+ot_logger.info("Finished UDP throughput tests")
+
+ot_logger.info("Leader packet counters, neighbour table, and error rates")
+log_ot_counters(ot_logger, ot_leader)
+log_ot_neighbour_table(ot_logger, ot_leader)
+
+ot_logger.info("Router 1 packet counters, neighbour table, and error rates")
+log_ot_counters(ot_logger, ot_router_1)
+log_ot_neighbour_table(ot_logger, ot_router_1)
+
+ot_logger.info("Router 2 packet counters, neighbour table, and error rates")
+log_ot_counters(ot_logger, ot_router_2)
+log_ot_neighbour_table(ot_logger, ot_router_2)
+
+ot_logger.info("Router 3 packet counters, neighbour table, and error rates")
+log_ot_counters(ot_logger, ot_router_3)
+log_ot_neighbour_table(ot_logger, ot_router_3)
+
+ot_leader.disconnect()
+ot_router_1.disconnect()
+ot_router_2.disconnect()
+ot_router_3.disconnect()
+ot_logger.info("Disconnected from all Raspberry Pis")
 
 logging.shutdown()
